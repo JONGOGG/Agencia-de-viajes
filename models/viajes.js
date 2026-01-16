b216b215ce666144bcc34472da3be6b72de506d7
JoanGzz <joangonzalez005@gmail.com>
2026-01-15 22:04:17 -0600

    Fix: Remove operatorsAliases and ensure PORT from env

--- models/viajes.js
+++ models/viajes.js
@@ -1,7 +1,7 @@
 import { DataTypes } from 'sequelize';
 import db from '../config/db.js';
 
-const Viaje = db.define('Viaje', {
+const Viajes = db.define('Viajes', {
   titulo: {
     type: DataTypes.STRING,
     allowNull: false
