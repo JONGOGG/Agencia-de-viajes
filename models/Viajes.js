8576e957a30e484dfe2ddedd01ba42c7a647eb11
JoanGzz <joangonzalez005@gmail.com>
2026-01-15 22:11:04 -0600

    Agrega modelos Viajes y Testimoniales correctamente

---
+++ models/Viajes.js
@@ -0,0 +1,42 @@
+import { DataTypes } from 'sequelize';
+import db from '../config/db.js';
+
+const Viajes = db.define('Viajes', {
+  titulo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  precio: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  fecha_ida: {
+    type: DataTypes.DATEONLY,
+    allowNull: false
+  },
+  fecha_vuelta: {
+    type: DataTypes.DATEONLY,
+    allowNull: false
+  },
+  imagen: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  descripcion: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  disponibles: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  slug: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: 'Viajes',  // ✅ Agrega esto
+  timestamps: false
+});
+
+export default Viajes;
